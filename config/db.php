23b61d7781a176bb456822e71e784758043f077d
yhssdl <yhssdl@163.com>
2020-10-17 13:45:49 +0800

    数据库名修改

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jnoj',
+    'dsn' => 'mysql:host=localhost;dbname=ojdate',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
