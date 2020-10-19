ae068da1fafd7785d484e2bb3b0db56abdc5f129
深蓝 <yhssdl@163.com>
2020-10-20 07:41:20 +0800

    加快代码提交后，显示结果的速度。

--- views/problem/view.php
+++ views/problem/view.php
@@ -437,7 +437,7 @@ if (waitingCount > 0) {
             interval = null;
         }
     }
-    interval = setInterval(testWaitingsDone, 20000);
+    interval = setInterval(testWaitingsDone, 3000);
 }
 EOF;
 $this->registerJs($js);
