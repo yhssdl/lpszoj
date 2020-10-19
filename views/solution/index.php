2a0e8a5a83dfeb88cca985f8b96ac89d08a5859e
深蓝 <yhssdl@163.com>
2020-10-20 07:46:44 +0800

    调整代码提交后，显示结果的速度

--- views/solution/index.php
+++ views/solution/index.php
@@ -162,7 +162,7 @@ if (waitingCount > 0) {
             interval = null;
         }
     }
-    interval = setInterval(testWaitingsDone, 200);
+    interval = setInterval(testWaitingsDone, 1000);
 }
 EOF;
 $this->registerJs($js);
