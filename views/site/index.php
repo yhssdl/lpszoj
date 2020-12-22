8c7683ac867178bc1901e63544547a14150c4dac
深蓝 <yhssdl@163.com>
2020-12-22 10:20:09 +0800

    首页新闻显示模式更改

--- views/site/index.php
+++ views/site/index.php
@@ -9,63 +9,54 @@ use yii\helpers\Html;
 $this->title = Yii::$app->setting->get('ojName');
 ?>
 <div class="row blog">
-    <div class="col-md-8">
-        <div class="jumbotron">
-            <h1>Hello, world!</h1>
-            <p>欢迎来到<?= Yii::$app->setting->get('schoolName') ?>在线判题系统——<?= Yii::$app->setting->get('ojName') ?> Online Judge</p>
-        </div>
-        <hr>
-        <div class="blog-main">
+<div class="col-lg-9 col-md-8">
+        <!-- <div class="d-none d-md-block">
+    <h3>新闻与公告</h3>
+    </div> -->
+        <div>
             <?php foreach ($news as $v): ?>
-                <div class="blog-post">
-                    <h2 class="blog-post-title"><?= Html::a(Html::encode($v['title']), ['/site/news', 'id' => $v['id']]) ?></h2>
-                    <p class="blog-post-meta">
-                        <span class="glyphicon glyphicon-time"></span> <?= Yii::$app->formatter->asDate($v['created_at']) ?></p>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title"><?= Html::a(Html::encode($v['title']), ['/site/news', 'id' => $v['id']], ['class' => 'text-dark']) ?>
+                    </h3>
+                    <?= Yii::$app->formatter->asMarkdown($v['content']) ?>
                 </div>
+                <div class="card-footer">
+                    <?= Yii::$app->formatter->asDate($v['created_at']) ?>
+                </div>
+            </div>
+            <p></p>
             <?php endforeach; ?>
             <?= \yii\widgets\LinkPager::widget([
                 'pagination' => $pages,
+                'maxButtonCount' => 5,
             ]); ?>
         </div>
+        <p></p>
     </div>
-    <div class="col-md-4">
+    <div class="col-lg-3 col-md-4">
         <div class="sidebar-module sidebar-module-inset">
             <h4>关于</h4>
             <p>Online Judge系统（简称OJ）是一个在线的判题系统。 用户可以在线提交程序多种程序（如C、C++、Java）源代码，系统对源代码进行编译和执行， 并通过预先设计的测试数据来检验程序源代码的正确性。</p>
         </div>
         <?php if (!empty($contests)): ?>
-        <div class="sidebar-module">
-            <h4>最近比赛</h4>
-            <ol class="list-unstyled">
-                <?php foreach ($contests as $contest): ?>
-                <li>
-                    <?= Html::a(Html::encode($contest['title']), ['/contest/view', 'id' => $contest['id']]) ?>
-                </li>
-                <?php endforeach; ?>
-            </ol>
-        </div>
+        <ol class="list-group">
+            <li class="list-group-item text-center"><i class="fas fa-fw fa-chart-line"></i>最近比赛</li>
+            <?php foreach ($contests as $contest): ?>
+            <?= Html::a(Html::encode($contest['title']), ['/contest/view', 'id' => $contest['id']], ['class' => 'list-group-item-action list-group-item']) ?>
+            <?php endforeach; ?>
+        </ol>
+        <!-- </div> -->
+        <p></p>
         <?php endif; ?>
         <?php if ((Yii::$app->setting->get('isDiscuss')) && (!empty($discusses))): ?>
-            <div class="sidebar-module">
-                <h4>最近讨论</h4>
-                <ol class="list-unstyled">
-                    <?php foreach ($discusses as $discuss): ?>
-                        <li class="index-discuss-item">
-                            <div>
-                                <?= Html::a(Html::encode($discuss['title']), ['/discuss/view', 'id' => $discuss['id']]) ?>
-                            </div>
-                            <small class="text-muted">
-                                <span class="glyphicon glyphicon-user"></span>
-                                <?= Html::a(Html::encode($discuss['nickname']), ['/user/view', 'id' => $discuss['username']]) ?>
-                                &nbsp;•&nbsp;
-                                <span class="glyphicon glyphicon-time"></span> <?= Yii::$app->formatter->asRelativeTime($discuss['created_at']) ?>
-                                &nbsp;•&nbsp;
-                                <?= Html::a(Html::encode($discuss['ptitle']), ['/problem/view', 'id' => $discuss['pid']]) ?>
-                            </small>
-                        </li>
-                    <?php endforeach; ?>
-                </ol>
-            </div>
+        <ol class="list-group">
+            <li class="list-group-item text-center"><i class="fas fa-fw fa-comment"></i>最近讨论</li>
+
+            <?php foreach ($discusses as $discuss): ?>
+            <?= Html::a(Html::encode($discuss['title']) . '<br /><small>' . Html::encode($discuss['nickname']) . ' ' . Yii::$app->formatter->asRelativeTime($discuss['created_at']) . ' ' . Html::encode($discuss['ptitle']) . '</small>', ['/discuss/view', 'id' => $discuss['id']], ['class' => 'list-group-item list-group-item-action']) ?>
+            <?php endforeach; ?>
+        </ol>
         <?php endif; ?>
     </div>
 </div>
