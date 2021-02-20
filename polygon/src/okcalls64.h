fb0de7a27ea5e843d0c5039eca0e2651aad9384b
深蓝 <yhssdl@163.com>
2021-02-20 22:45:58 +0800

    修正ununtu 20.04LTS一键安装后，JAVA与Python语言运行出错

--- polygon/src/okcalls64.h
+++ polygon/src/okcalls64.h
@@ -21,7 +21,7 @@ int LANG_CV[256] = {0,1,2,3,4,5,8,9,10,11,12,20,21,59,63,89,99,158,202,231,240,2
                     		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
                     		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 0 };
 //java
-int LANG_JV[256] = {0,2,3,4,5,9,10,11,12,13,14,21,56,59,89,97,104,157,158,202,218,231,273,257,
+int LANG_JV[256] = {0,2,3,4,5,9,10,11,12,13,14,17,21,56,59,89,97,104,157,158,202,218,231,273,257,
 		61, 22, 6, 33, 8, 13, 16, 111, 110, 39, 79, 302, SYS_fcntl,
 		SYS_getdents64, SYS_getrlimit, SYS_rt_sigprocmask, SYS_futex, SYS_read,
 		SYS_mmap, SYS_stat, SYS_open, SYS_close, SYS_execve, SYS_access,
@@ -30,7 +30,7 @@ int LANG_JV[256] = {0,2,3,4,5,9,10,11,12,13,14,21,56,59,89,97,104,157,158,202,21
 		SYS_getuid, SYS_getgid, SYS_geteuid, SYS_getegid, SYS_set_thread_area,
 		SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 158, 0 };
 //python
-int LANG_YV[256] = {0,2,3,4,5,6,8,9,10,11,12,13,14,16,21,32,59,72,78,79,89,97,99,102,104,107,108,131,158,217,218,228,231,272,273,318,39,99,302,99,32,72,131,1,202,257,41, 42, 146, SYS_mremap, 158, 117, 60, 39, 102, 191,
+int LANG_YV[256] = {0,2,3,4,5,6,8,9,10,11,12,13,14,16,17,21,32,59,72,78,79,89,97,99,102,104,107,108,131,158,217,218,228,231,272,273,318,39,99,302,99,32,72,131,1,202,257,41, 42, 146, SYS_mremap, 158, 117, 60, 39, 102, 191,
                     		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_execve,
                     		SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_futex, SYS_getcwd,
                     		SYS_getdents, SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getrlimit,
