cd336cdcf29d2af8a81772c5610f7b404fcb52c8
深蓝 <yhssdl@163.com>
2021-02-21 16:16:17 +0800

    一键安装更新

--- docs/install.sh
+++ docs/install.sh
@@ -124,25 +124,27 @@ error_detect_depends(){
 
 install_dependencies(){
     if check_sys packageManager yum; then
-        echo -e "[${green}Info${plain}] Checking the EPEL repository..."
+        echo -e "[${green}Info${plain}] Checking the EPEL repository..."        
+        
+        sudo dnf -y install dnf-plugins-core
+        sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        sudo dnf config-manager --set-enabled PowerTools
+
+        error_detect_depends "yum -y install gcc-c++ git make gcc glibc-static libstdc++-static"
+
         yum install -y epel-release
+        yum install -y http://rpms.remirepo.net/enterprise/remi-release-8.rpm
         [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "[${red}Error${plain}] Install EPEL repository failed, please check it." && exit 1
         [ ! "$(command -v yum-config-manager)" ] && yum install -y yum-utils > /dev/null 2>&1
         [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel > /dev/null 2>&1
 
-        yum install -y http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-        yum install -y yum-utils
-        sudo dnf -y install dnf-plugins-core
-        sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-        sudo dnf config-manager --set-enabled PowerTools
-        
+
         echo -e "[${green}Info${plain}] Checking the EPEL repository complete..."
 
         yum_depends=(
             nginx
             php74-php-cli php74-php-fpm php74-php-gd php74-php-mbstring php74-php-mysqlnd php74-php-xml
             mariadb mariadb-devel mariadb-server
-            gcc-c++ git make gcc glibc-static libstdc++-static
             java-1.8.0-openjdk java-1.8.0-openjdk-devel
             python38
         )
