d5e7fd5e3b5954c1d8565f0473d4e23dd08a32ae
深蓝 <yhssdl@163.com>
2021-06-15 19:24:10 +0800

    数据库相关修改

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ojdate',
-    'username' => 'root',
+    'username' => 'ojdate',
     'password' => '123456',
     'charset' => 'utf8',
 
