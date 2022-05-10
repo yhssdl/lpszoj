101358af1f1d735711ebe9d9c60dc532f4526eb1
深蓝 <yhssdl@163.com>
2022-05-10 21:24:46 +0800

    精简了CKEditor的文件。

--- widgets/ckeditor/assets/build-config.js
+++ widgets/ckeditor/assets/build-config.js
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/933a46a9639ffffc853fa7bff34535ba
+ * (2) https://ckeditor.com/cke4/builder/66338f550e5d5af4789f1ac0fc294a19
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/933a46a9639ffffc853fa7bff34535ba
+ * (3) https://ckeditor.com/cke4/builder/download/66338f550e5d5af4789f1ac0fc294a19
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -54,16 +54,11 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
-		'a11yhelp' : 1,
-		'about' : 1,
 		'basicstyles' : 1,
-		'blockquote' : 1,
 		'clipboard' : 1,
-		'codeTag' : 1,
 		'codesnippet' : 1,
 		'colorbutton' : 1,
 		'contextmenu' : 1,
-		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
@@ -71,28 +66,13 @@ var CKBUILDER_CONFIG = {
 		'font' : 1,
 		'format' : 1,
 		'horizontalrule' : 1,
-		'htmlwriter' : 1,
 		'image' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
-		'magicline' : 1,
 		'maximize' : 1,
-		'pastefromgdocs' : 1,
-		'pastefromlibreoffice' : 1,
-		'pastefromword' : 1,
-		'pastetext' : 1,
-		'removeformat' : 1,
 		'resize' : 1,
-		'scayt' : 1,
-		'showborders' : 1,
 		'sourcearea' : 1,
-		'specialchar' : 1,
-		'stylescombo' : 1,
-		'tab' : 1,
-		'table' : 1,
-		'tableselection' : 1,
-		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'uploadimage' : 1,
--- widgets/ckeditor/assets/config.js
+++ widgets/ckeditor/assets/config.js
@@ -3,19 +3,17 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
 		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'tools', groups: [ 'tools' ] },
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Scayt,Anchor,SpecialChar,Strike,Underline,Outdent,Indent,Styles,Font,About';
-	config.extraPlugins = 'uploadimage';
-	config.filebrowserImageUploadUrl ='/image/upload';
-}
+	config.removeButtons = 'Underline,Subscript,Superscript,Font,Cut,Copy,Paste,Outdent,Indent,Anchor';
+};
