d5dddc8ad333edb9c136ad8601ab71589ba41b51
深蓝 <yhssdl@163.com>
2023-12-15 22:17:58 +0800

    处理反序列号错误时的情况。

--- models/Problem.php
+++ models/Problem.php
@@ -162,7 +162,6 @@ class Problem extends ActiveRecord
     public function setSamples()
     {
         try{
-            $this->sample_output = "skdf";
             $input = unserialize($this->sample_input ?? '');
             $output = unserialize($this->sample_output ?? '');
         }catch(\Throwable $e){
