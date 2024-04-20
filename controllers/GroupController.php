7f41047dc4216d98741b745dc6fa9e491e7f3ed0
深蓝 <yhssdl@163.com>
2024-04-20 20:13:47 +0800

    结业小组中克隆功能的目标改为我的小组。

--- controllers/GroupController.php
+++ controllers/GroupController.php
@@ -259,20 +259,10 @@ class GroupController extends BaseController
 
 
         if (!Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()) {
-            if($model->status==Group::STATUS_GRADUATION) {
-                $sql1 = "g.status=:status";
-            }else{
-                $sql1 = "g.status<>:status";
-            }
-            $group_datas = Yii::$app->db->createCommand('SELECT g.id,g.name FROM {{%group}}  AS g LEFT JOIN {{%group_user}} AS u ON u.group_id=g.id WHERE g.id<>:gid AND u.user_id=:id AND is_train=:is_train AND '.$sql1,
+            $group_datas = Yii::$app->db->createCommand('SELECT g.id,g.name FROM {{%group}}  AS g LEFT JOIN {{%group_user}} AS u ON u.group_id=g.id WHERE g.id<>:gid AND u.user_id=:id AND is_train=:is_train AND g.status<>:status',
             [':gid' => $model->id,':id' => Yii::$app->user->id,':is_train' => Group::MODE_GROUP,':status'=>Group::STATUS_GRADUATION])->queryAll();       
         }else if($model->hasPermission()){
-            if($model->status==Group::STATUS_GRADUATION) {
-                $sql1 = "status=:status";
-            }else{
-                $sql1 = "status<>:status";
-            }
-            $group_datas = Yii::$app->db->createCommand('SELECT id,name FROM {{%group}}  WHERE id<>:gid AND created_by=:id AND is_train=:is_train AND '.$sql1,
+            $group_datas = Yii::$app->db->createCommand('SELECT id,name FROM {{%group}}  WHERE id<>:gid AND created_by=:id AND is_train=:is_train AND status<>:status',
             [':gid' => $model->id,':id' => Yii::$app->user->id,':is_train' => Group::MODE_GROUP,':status'=>Group::STATUS_GRADUATION])->queryAll();         
         } else{
             $group_datas = null;
